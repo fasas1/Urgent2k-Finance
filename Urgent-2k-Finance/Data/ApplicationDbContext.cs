4034f44aa5fd83e4b47505b8469a7a6deac52461
Fasasi Qasim <fasaqa1000@gmail.com>
2024-10-18 15:06:43 +0100

    basic setup

---
+++ Urgent-2k-Finance/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Urgent_2k_Finance.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions option) :base(option) { }
+      
+    }
+}
